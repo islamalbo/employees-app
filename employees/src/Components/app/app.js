064ea32bc22c76d1ec2c21c2c68da4c6957e099b
Islam Albogachiev <islam.albogachiev.2016@mail.ru>
2022-07-31 20:09:32 +0300

    added components for every block

---
+++ employees/src/Components/app/app.js
@@ -0,0 +1,28 @@
+import AppInfo from '../app-info/app-info';
+import SeacrhPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+            
+            <div className="search-panel">
+                <SeacrhPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList/>
+
+            <EmployeesAddForm/>
+        </div>
+
+        
+    )
+}
+
+export default App;
